5a138732a0724fb12dad5f1bd761841073af86d5
boran <sean@boran.com>
2013-11-08 16:41:14 +0100

    Composer: no doctrine. config.php: explain access restrictions.

--- web/config.php
+++ web/config.php
@@ -10,8 +10,8 @@ if (!in_array(@$_SERVER['REMOTE_ADDR'], array(
     '::1',
 ))) {
     header('HTTP/1.0 403 Forbidden');
-    exit('This script is only accessible from localhost.');
-}
+    exit('This script is only accessible from localhost, your IP is ' . $_SERVER['REMOTE_ADDR']
+      . ' Check '.basename(__FILE__).' for more information.' );}
 
 require_once dirname(__FILE__).'/../app/SymfonyRequirements.php';
 
